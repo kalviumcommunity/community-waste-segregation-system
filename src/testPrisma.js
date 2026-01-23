d980980646f45696208cf33b2537fdb27018c080
unknown <ponyteju995@gmail.com>
2026-01-23 14:48:40 +0530

    Transaction & Query Optimisation

---
+++ src/testPrisma.js
@@ -0,0 +1,16 @@
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const users = await prisma.user.findMany();
+  console.log('Users from DB:', users);
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
