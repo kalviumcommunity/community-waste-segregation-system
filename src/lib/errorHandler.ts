f2620b6070d652bc11a70b08bf17d9cce95bcd1c
sharon-Emmanuel-sr <sharonmass75@gmail.com>
2026-01-30 14:51:22 +0530

    2.22 Error Handling Middleware

--- src/lib/errorHandler.ts
+++ src/lib/errorHandler.ts
@@ -4,11 +4,10 @@ import { NextResponse } from "next/server";
 export function handleError(error: any) {
 console.error(error);
 
-
 return NextResponse.json(
 {
 success: false,
-message: "Internal Server Error",
+message: "Internal Server Errorr",
 },
 { status: 500 }
 );
