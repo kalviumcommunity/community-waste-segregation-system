44be5b918d829fd94039add224bd37a026c24608
sharon-Emmanuel-sr <sharonmass75@gmail.com>
2026-01-30 14:33:12 +0530

    Complete Sprint 1 Step 2.17 – API route structure and naming

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,15 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
+
 export function middleware(request: NextRequest) {
-  return NextResponse.next();
+const token = request.cookies.get("token")?.value;
+
+
+if (!token) {
+return NextResponse.redirect(new URL("/login", request.url));
+}
+
+
+return NextResponse.next();
 }
